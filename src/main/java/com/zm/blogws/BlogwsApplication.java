f1f5fa9988eaaba8634077fa89767125df34f062
mohamed <mohamed.zerdan@e-polytechnique.ma>
2020-09-26 17:56:30 +0100

    initial commit : blog api

---
+++ src/main/java/com/zm/blogws/BlogwsApplication.java
@@ -0,0 +1,13 @@
+package com.zm.blogws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zm/blogws/BlogwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zm.blogws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
