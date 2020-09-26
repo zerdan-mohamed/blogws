cdca0e2c49182c717f39f4470eed92a9bdbeba26
mohamed <mohamed.zerdan@e-polytechnique.ma>
2020-09-26 18:40:36 +0100

    Configure database & create entities

---
+++ src/main/java/com/zm/blogws/model/Post.java
@@ -0,0 +1,77 @@
+package com.zm.blogws.model;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Lob
+    @Column
+    private String content;
+
+    @Column
+    private Instant createdOn;
+
+    @Column
+    private Instant updatedOn;
+
+    @Column
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Instant getCreatedOn() {
+        return createdOn;
+    }
+
+    public Instant getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCreatedOn(Instant createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public void setUpdatedOn(Instant updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/com/zm/blogws/model/User.java
@@ -0,0 +1,53 @@
+package com.zm.blogws.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
