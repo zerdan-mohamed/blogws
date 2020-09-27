645155da1f36eb0ef45ac8212bd65e2d9d4db6a7
mohamed <mohamed.zerdan@e-polytechnique.ma>
2020-09-27 01:15:24 +0100

    MZN003 Encoding passwords using spring security

--- src/main/java/com/zm/blogws/service/AuthService.java
+++ src/main/java/com/zm/blogws/service/AuthService.java
@@ -4,6 +4,7 @@ import com.zm.blogws.dto.RegisterRequest;
 import com.zm.blogws.model.User;
 import com.zm.blogws.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,17 +12,26 @@ public class AuthService {
 
     private UserRepository userRepository;
 
-    public AuthService(UserRepository userRepository) {
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
 
         user.setUsername(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
         user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
 
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
